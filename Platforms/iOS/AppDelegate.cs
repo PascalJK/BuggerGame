170f944fc9ce704294de0b9438f49b2267581ee6
Pascal JK <pascual.santos80@gmail.com>
2022-12-11 10:27:28 +0200

    Add project files.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace BuggerGame;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
